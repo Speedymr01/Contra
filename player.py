9afe6afbc2da200819bc68e77d0eb4af614639c6
Speedymr01 <speedyfizz09@outlook.com>
2024-10-30 12:14:09 +0000

    Collisions

--- player.py
+++ player.py
@@ -4,7 +4,7 @@ from pygame.math import Vector2 as vector
 from os import walk
 
 class Player(pygame.sprite.Sprite):
-    def __init__(self, pos, groups, path):
+    def __init__(self, pos, groups, path, collision_sprites):
         super().__init__(groups)
         self.import_assets(path)
         self.frame_index = 0
@@ -16,6 +16,9 @@ class Player(pygame.sprite.Sprite):
         self.direction = vector()
         self.pos = vector(self.rect.topleft)
         self.speed = 400
+
+        self.old_rect = self.rect.copy()
+        self.collision_sprites = collision_sprites
     
     def import_assets(self, path):
         self.animations = {}
@@ -46,9 +49,11 @@ class Player(pygame.sprite.Sprite):
     def move(self, dt):
         self.pos.x += self.direction.x * self.speed * dt
         self.rect.x = round(self.pos.x)
+        self.collision('horizontal')
 
         self.pos.y += self.direction.y * self.speed * dt
         self.rect.y = round(self.pos.y)
+        self.collision('vertical')
     
     def animate(self, dt):
         self.frame_index += 7 * dt
@@ -56,7 +61,36 @@ class Player(pygame.sprite.Sprite):
             self.frame_index = 0
         self.image = self.animations[self.status][int(self.frame_index)]
 
+    def collision(self, direction):
+        for sprite in self.collision_sprites.sprites():
+            if sprite.rect.colliderect(self.rect):
+                if direction == 'horizontal':
+                    # left collision
+                    if self.rect.left <= sprite.rect.right and self.old_rect.left >= sprite.old_rect.right:
+                        self.rect.left = sprite.rect.right
+                    # right collision
+                    if self.rect.right >= sprite.rect.left and self.old_rect.right <= sprite.old_rect.left:
+                        self.rect.right = sprite.rect.left
+
+
+                    self.pos.x = self.rect.x
+
+                else: pass
+                if direction == 'vertical':
+                    # top collision
+                    if self.rect.top <= sprite.rect.bottom and self.old_rect.top >= sprite.old_rect.bottom:
+                        self.rect.top = sprite.rect.bottom
+                    # botom collision
+                    if self.rect.bottom >= sprite.rect.top and self.old_rect.bottom <= sprite.old_rect.top:
+                        self.rect.bottom = sprite.rect.top
+
+
+                    self.pos.y = self.rect.y
+
+                else: pass
+
     def update(self, dt):
+        self.old_rect = self.rect.copy()
         self.input()
         self.move(dt)
         self.animate(dt)
